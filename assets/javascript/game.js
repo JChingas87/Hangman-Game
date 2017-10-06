c0976102ccafaf0d70c89f3c78b2b1c421260824
Joseph A Chingas <JChingas87@Josephs-MacBook-Pro.local>
2017-10-06 16:02:12 -0400

    please help

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,76 @@
-// Press any key to start the game    document.onkeyup = function(event) {
-
-// generate hidden word at random - create array
-
+//
+//
+//
 var gamewords = ["football", "creative", "universe", "theory", "karate", "program", "stoic", "meditate", "guitar", "katana", "fitness", "driving", "planning", "finance", "reading", "discipline", "hiking", "forest", "wisdom", "courage"];
 var wins = 0;
 var remainingguesses = 10;
-var lettersguessed = "";
 var randomwords = gamewords[Math.floor(Math.random() * gamewords.length)];
 console.log(randomwords)
+var chosenWord = randomwords;
+var correctWord = "";
 
 
 document.onkeyup = function(event) {
 
+
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
+    function myFunction() {
+        var str = document.getElementById("current-word").innerHTML;
+        var res = str.replace("current-word", userGuess);
+        document.getElementById("current-word").innerHTML = res;
 
-    if ((userGuess) == (randomwords.charAt(0)) || ((userGuess) == (randomwords.charAt(1, 9))) || ((userGuess) == (randomwords.charAt(2))) || ((userGuess) == (randomwords.charAt(3))) || ((userGuess) == (randomwords.charAt(4))) || ((userGuess) == (randomwords.charAt(5))) || ((userGuess) == (randomwords.charAt(6))) || ((userGuess) == (randomwords.charAt(7))) || ((userGuess) == (randomwords.charAt(8))) || ((userGuess) == (randomwords.charAt(9)))) {
-        wins++;
-        remainingguesses--;
-        console.log(userGuess);
-        console.log(wins);
-        console.log(remainingguesses);
-    } else {
-        remainingguesses--;
-        console.log(userGuess);
-        console.log(remainingguesses);
+
+
+        if ((userGuess) == (chosenWord.charAt(0)) || ((userGuess) == (chosenWord.charAt(1, 9))) || ((userGuess) == (chosenWord.charAt(2))) || ((userGuess) == (chosenWord.charAt(3))) || ((userGuess) == (chosenWord.charAt(4))) || ((userGuess) == (chosenWord.charAt(5))) || ((userGuess) == (chosenWord.charAt(6))) || ((userGuess) == (chosenWord.charAt(7))) || ((userGuess) == (chosenWord.charAt(8))) || ((userGuess) == (chosenWord.charAt(9)))) {
+            wins++;
+            remainingguesses--;
+            var guesses = userGuess;
+            guesses = document.getElementById("guesses");
+            guesses.innerHTML = remainingguesses;
+
+            var letters = userGuess;
+            letters = document.getElementById("letters");
+            letters.innerHTML += userGuess + ", ";;
+
+        } else {
+            remainingguesses--;
+            var letters = userGuess;
+            letters = document.getElementById("letters");
+            letters.innerHTML += userGuess + ", ";
+            var guesses = userGuess;
+            guesses = document.getElementById("guesses");
+            guesses.innerHTML = remainingguesses;
+
+        }
+
+        function playerWins() {}
+        if (("current-word") === (correctWord)) {
+            var userWins = wins;
+            userWins = document.getElementById("wins");
+            userWins.innerHTML = wins++;
+        }
     }
+    myFunction();
+}
 
-    if ((remainingguesses) == 0) {
+// window.location.reload(false);
+function resetgame() {
+    return gamewords;
+    return randomwords;
 
-        console.log("you lose");
+    if (remainingguesses === 0) {
+        console.log("You lost")
+
+        chosenWord = randomwords;
     }
-
-    // for (i = 0; i < randomwords.length; i++) {
-    //     if ((userGuess) == (randomwords.charAt(0))) {
-    //         wins++;
-    //         remainingguesses--;
-    //         console.log(userGuess);
-    //         console.log(wins);
-    //         console.log(remainingguesses);
-    //     } else {
-    //         remainingguesses--;
-    //         console.log(userGuess);
-    //         console.log(remainingguesses);
-    //     }
-    // }
 
 }
+
+// function get_random_word () {
+//     return array_for_randompull[randomizer];
+// }
+// if (missed_guesses == 0){
+//     console.log("You fuckin lost")
+//     word_to_evaluate = get_random_word;
+// }
